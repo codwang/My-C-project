6496199c3e80a84c2691a054412721bd17cb0fb4
codwang <1309357424@qq.com>
2020-11-09 19:27:55 +0800

    C++入门

---
+++ 2020.09.14/引用for each循环.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	//int arr[5] = {1,2,3,4,5};
+//	//for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+//	//	arr[i]*=2;
+//	//for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+//	//	cout << arr[i]<<" " ;
+//	//cout << endl;
+//	//for(auto&p:arr)
+//	//	cout <<p<< " ";
+//	int a = 10;
+//	//auto& b = a;
+//	//const double& b = a;
+//	const int&b = 10;
+//	cout << b << endl;
+	 //最好使用nullptr 表示指针空值
+//cout << sizeof(nullptr) << endl;
+//cout << sizeof((void*)0) << endl;//nullptr_t是一种类型
+
+//
+//}
