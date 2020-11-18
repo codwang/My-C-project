670f7c29c659997a96b8adb99f663013355343bb
codwang <1309357424@qq.com>
2020-11-18 23:59:05 +0800

    类的定义和空类

---
+++ 2020.09.24/2020.09.24.cpp
@@ -0,0 +1,78 @@
+//#include "Person.h"
+#include<iostream>
+#pragma warning(disable:4996)
+using namespace std;
+//class Student
+//{
+//public:
+//	const char *name; //姓名
+//	const char* sex;//性别
+//	int age;//年龄
+//	 //char _id[10];
+//public:
+//	//显示基本信息
+//	//void ShowInfo()
+//	//{
+//	//	cout <<_id<<" "<<name << " " << sex <<" "<<age << endl;
+//	//}
+//	//void Getid(char id[])
+//	//{
+//	//	strcpy(_id, id);
+//	//}
+//};
+class Student1
+{
+	void f()
+	{
+
+	}
+};
+class Student2
+{
+	//编译器给了空类一个字节来唯一标识这个类
+};
+//void Person::InitPerson(const char *name, const char *gender, int age)
+//{
+//	strcpy(_name, name);
+//	strcpy(_gender, gender);
+//	_age = age;
+//}
+//void Person::Eat()
+//{
+//	cout << "eat" << endl;
+//}
+//void Person::Sleep()
+//{
+//	cout << "sleep" << endl;
+//}
+//void Person::PrintPersonInfo()
+//{
+//	cout << _name << " " << _gender << " " << _age << endl;
+//}
+//int main()
+//{
+//	int i = 10;
+//	i = i++;
+//	cout << i << endl;
+//	Student s ;
+//	//s.name = "zhangsan";
+//	//s.sex = "男";
+//	//s.age = 16;
+//	
+//	//s.ShowInfo();
+//	//cout << sizeof(Student) << endl;
+//	//cout << sizeof(Student1) << endl;
+//	//cout << sizeof(Student2) << endl;
+//	//cout << sizeof(char*) << endl;
+//	//cout << sizeof(int) << endl;
+//	Person p;
+//	//p.InitPerson("张三","男",15);
+//	////strcpy(p._name, "zhangsan");
+//	//p.Sleep();
+//	//p.PrintPersonInfo();
+//	//strcpy(s._id, "asda");
+//	//cout << sizeof(p) << endl;
+//	//student2 s1, s2;
+//	
+//	return 0;
+//}
